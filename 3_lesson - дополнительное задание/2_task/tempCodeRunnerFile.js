10bba290208335985a19575981fb158630b08089
Wograin <enkilart@mail.ru>
2019-04-27 20:21:22 +0300

    Урок 5: дополнительное задание

--- 3_lesson - дополнительное задание/2_task/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-function foo(val) {
-    let string = val.trim();
-    if (typeof (string) != "string") {
-        return "Передана не строка !";
-    } else {
-        return "Передана строка !";
-    }
-    console.log(string);
-}
---
+++ 5_lesson - дополнительное задание/2_task/js_lesson_5.js
@@ -0,0 +1,59 @@
+let dayOne = new Date(2018, 5, 30, 9, 59, 59);
+
+function date(date) {
+
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let seconds = date.getSeconds();
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    let day = date.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    let month = date.getMonth();
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    let year = date.getFullYear();
+
+    return (hours + ":" + minutes + ":" + seconds + " " + day + "." + month + "." + year);
+}
+//------------------------------------------------------------------------------------------------------------------------
+
+let dayTwo = new Date(2018, 6, 1);
+
+function newDate(date) {
+
+    let day = date.getDate();
+    if (day < 10) {
+        day = "0" + day;
+    }
+
+    let month = date.getMonth();
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    let year = date.getFullYear();
+
+    return (day + "." + month + "." + year);
+}
+//------------------------------------------------------------------------------------------------------------------------
+
+let toDay = new Date(),
+    formatter = new Intl.DateTimeFormat("ru", {
+        weekday: "long"
+    });
